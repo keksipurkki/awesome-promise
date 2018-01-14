31064118656351e4918c305dec9467421ff6e03e
Elias Toivanen <elias.a.toivanen@gmail.com>
2018-01-15 00:31:30 +0200

    remove commented code

--- throttled.js
+++ throttled.js
@@ -23,41 +23,14 @@ function throttled(f) {
   };
 }
 
-// Some asynchronous function
 const randomTime = (min = 0, max = 1000) => () => {
   return Math.random()*(max - min) + min;
 };
 
 const sleep = randomTime(0, 3000);
 
+// Some asynchronous function
 const fun = input => new Promise(resolve => setTimeout(resolve, sleep(), input.toUpperCase()));
-
-//
-//const apply = (acc, val) => acc.then(val);
-//const compose = (...funcs) => x => funcs.reduce(apply, Promise.resolve(x));
-//const queue = (f, inputs) => compose(...inputs.map(input => () => f(input)))();
-//
-//let outputs = [];
-//
-//const store = output => {
-//  outputs.push(output);
-//  return output;
-//};
-//
-//const throttle = input => {
-//  const [seconds, nanos] = process.hrtime(start);
-//  const callRate = callCount++ / seconds;
-//  if (!isNaN(callRate) && callRate < throttleThreshold)
-//    return Promise.resolve(input);
-//  return new Promise(resolve =>
-//    setTimeout(resolve, 1000 * (callRate - throttleThreshold), input)
-//  );
-//};
-//
-//queue(compose(throttle, fun, store, console.log), inputs).then(() => {
-//  console.log(`Resolved: ${outputs}`);
-//});
-
 
 const toc = +new Date();
 Promise.all(inputs.map(throttled(fun)))
